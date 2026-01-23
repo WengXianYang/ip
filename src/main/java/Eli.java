fb323bc55f1e3070efe7afd14453672a626158c0
Weng Xian Yang <e1398191@u.nus.edu>
2026-01-23 22:48:08 +0800

    Change Duke to Eli

---
+++ src/main/java/Eli.java
@@ -0,0 +1,7 @@
+public class Eli {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Eli");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
