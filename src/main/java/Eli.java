6bdd49018cfd15e21c2c9b5e44f9979c3de202b7
Weng Xian Yang <e1398191@u.nus.edu>
2026-01-27 21:19:04 +0800

    Add logo and and implement echo/exit commands

--- src/main/java/Eli.java
+++ src/main/java/Eli.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
+
 public class Eli {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Eli");
+        String logo = " _____ _     ___ \n"
+            + "           | ____| |   |_ _|\n"
+            + "           |  _| | |    | | \n"
+            + "           | |___| |___ | | \n"
+            + "           |_____|_____|___|\n";
+        System.out.println("Hello! I'm " + logo);
         System.out.println("What can I do for you?");
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("_________________________________");
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                break;
+            }
+            System.out.println("_________________________________");
+            System.out.println(line);
+        }
+        System.out.println("_________________________________");
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
